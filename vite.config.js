361f93ac12b4818880df5da2c44eb986df600df4
tchiinhemba <tchiinhemba@outlook.com>
2022-04-30 09:26:21 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 const path = require('path')
 
+function getSystemPath() {
+  const string = path.resolve(__dirname);
+
+
+  if(string.startsWith('C')) {
+    return `./src/styles/variables.scss`
+  } else {
+    return path.resolve(__dirname, 'src', 'styles', 'variables.scss');
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "${path.resolve(__dirname, 'src', 'styles', 'variables.scss')}";`
+        additionalData: `@import "${getSystemPath()}";`
       }
     }
   }
